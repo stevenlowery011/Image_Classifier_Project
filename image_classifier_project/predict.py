96c12c07395b45ed63b903e5953ffa0b33834f65
Steven Lowery <31108573+stevenlowery011@users.noreply.github.com>
2018-06-12 22:45:19 -0400

    updates to train.py. Argparse completed for predict.py

--- image_classifier_project/predict.py
+++ image_classifier_project/predict.py
@@ -2,8 +2,32 @@
 predict.py docstring
 """
 
+import argparse
+
 def main():
-    pass
+    in_arg = get_input_args()
+    print(in_arg)
+
+
+
+def get_input_args():
+    """
+    docstring
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', action="store", type=str, 
+                        help='path to input image file')
+    parser.add_argument('checkpoint', action="store", type=str, default='checkpoint.pth',
+                        help='a checkpoint file *.pth of a trained network')
+    parser.add_argument('--top_k', type=int, default=5,
+                        help='top k results from classifier')
+    parser.add_argument('--category_names', type=str, default='cat_to_name.json',
+                        help='a *.json file mapping categories to names')
+    parser.add_argument('--gpu', action="store_true", default=False,
+                        help='pass this argument to use GPU, else use CPU')
+    return parser.parse_args()
+
+
 
 if __name__ == '__main__':
     main()
